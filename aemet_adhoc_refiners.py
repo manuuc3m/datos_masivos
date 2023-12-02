d57b127ba2892a3e311e8dc3fdb89edbe62a0b41
jonyanvar <100363885@alumnos.uc3m.es>
2023-12-02 11:09:42 +0100

    Tabla aemet montada. Falta arreglar parte de la misma con los estilos de Borao

--- aemet_adhoc_refiners.py
+++ aemet_adhoc_refiners.py
@@ -1,24 +1,24 @@
 def refine_precipitacion(data):
     precipitationData = [f"{e['value']} %" for e in data]
     if ( len(precipitationData) < 7):
-        diff = ['0 %']*(7 - len(precipitationData))
+        diff = [' - ']*(7 - len(precipitationData))
         return precipitationData + diff
     return precipitationData
 
 def refine_nieve(nievedata):
     values = [e['value'] for e in nievedata]
     print('values', values)
-    mapping_empties = [ 'Sin nieve' if e == '' else f"{e} m" for e in values]
+    mapping_empties = [ ' - ' if e == '' else f"{e} m" for e in values]
     # Necesario que sean siempre 7 valores
     if (len(mapping_empties) < 7):
-        diff = ['Sin nieve'] * ( 7 - len(mapping_empties) )
+        diff = [' - '] * ( 7 - len(mapping_empties) )
         return mapping_empties + diff
     return mapping_empties
 
 def refine_nubosidad(nubosdata):
     descriptions = [e['descripcion'] if e != '' else 'Despejado' for e in nubosdata]
     if (len(descriptions) < 7):
-        diff = ['Sin nubes'] * ( 7 - len(descriptions) )
+        diff = [' - '] * ( 7 - len(descriptions) )
         return descriptions + diff
     return descriptions
 
@@ -42,6 +42,6 @@ def refine_viento(vientodata):
         } for e in vientodata
     ]
     if (len(info) < 7):
-        diff = [{'direccion': 'No hay viento', 'velocidad': '0 Km/h'}] * ( 7 - len(info) )
+        diff = [{'direccion': ' - ', 'velocidad': ' - '}] * ( 7 - len(info) )
         return info + diff
     return info
